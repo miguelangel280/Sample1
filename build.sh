33f411c5ec35b44335f21cb22c0095758749c798
miguelangel280 <almacenlogistica@hotmail.com>
2015-11-19 16:39:02 -0500

    first commit

--- build.sh
+++ build.sh
@@ -1,10 +1,3 @@
-
-javac FileHandling.java
-pause
-jar cvfm FileHandling.jar manifest.mft *.class
-del *.class
-
-
-#javac *.java
-#java FileHandling
+javac StringCalculator.java
+java  StringCalculator
 exit
